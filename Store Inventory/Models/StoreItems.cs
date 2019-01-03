82fb7f06ab3c5278264f1be668823a0a46b70a70
Georgy Jose <georgyjose95@gmail.com>
2019-01-03 17:00:30 +0000

    Model Modification

--- Store Inventory/Models/StoreItems.cs
+++ Store Inventory/Models/StoreItems.cs
@@ -8,12 +8,14 @@ namespace Store_Inventory.Models
 {
     public class StoreItems
     {
-            [Key]
-            public int ItemId { get; set; }
-
-            public String ItemName { get; set; }
-            public int? ItemQuantity { get; set; }
-            public double ItemPrice { get; set; }
+        [Key]
+        public int ItemId { get; set; }
+        [Display(Name ="Item Name")]
+        public String ItemName { get; set; }
+        [Display(Name = "Item Quantity")]
+        public int? ItemQuantity { get; set; }
+        [Display(Name = "Item Price")]
+        public double ItemPrice { get; set; }
 
     }
 }
